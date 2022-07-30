c6613722a5ff0483a530b4da0fe077fcb2be2f86
Nick Cao <nickcao@nichi.co>
2022-07-31 01:05:36 +0800

    add logging

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use async_recursion::async_recursion;
 use std::future::Future;
 use swayipc::{Connection, Event, EventType, Node, NodeType, WindowChange};
 use zbus::zvariant;
-use zbus_systemd::systemd1::UnitProxy;
+use zbus_systemd::systemd1::{ManagerProxy, ScopeProxy, UnitProxy};
 
 #[async_recursion]
 async fn walk_tree<'a, 'b: 'a, T>(
@@ -13,61 +13,87 @@ async fn walk_tree<'a, 'b: 'a, T>(
         + std::marker::Sync
         + std::marker::Send
         + 'async_recursion,
-) where
-    T: Future<Output = ()> + 'b + std::marker::Send + 'async_recursion,
+) -> anyhow::Result<()>
+where
+    T: Future<Output = anyhow::Result<()>> + 'b + std::marker::Send + 'async_recursion,
 {
-    apply(&root).await;
+    apply(&root).await?;
     for node in root.nodes.iter() {
-        walk_tree(&node, apply).await;
+        walk_tree(&node, apply).await?;
     }
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    simple_logger::init_with_level(log::Level::Info)?;
     for event in Connection::new()?.subscribe([EventType::Window])? {
         match event? {
             Event::Window(w) => match w.change {
-                WindowChange::Focus => {
-                    let mut conn = Connection::new()?;
-                    walk_tree(&conn.get_tree()?, async move |node| {
-                        let conn = zbus::Connection::session().await.unwrap();
-                        let manager = zbus_systemd::systemd1::ManagerProxy::new(&conn)
-                            .await
-                            .unwrap();
-                        match node.node_type {
-                            NodeType::Con | NodeType::FloatingCon => {
-                                if let Ok(unit) = manager
-                                    .get_unit_by_pid(node.pid.unwrap().try_into().unwrap())
-                                    .await
-                                {
-                                    let unit = UnitProxy::builder(&conn)
-                                        .path(unit)
-                                        .unwrap()
-                                        .build()
-                                        .await
-                                        .unwrap();
-                                    let quota = if node.visible.unwrap() {
-                                        u64::MAX
+                WindowChange::New
+                | WindowChange::Move
+                | WindowChange::Close
+                | WindowChange::FullscreenMode
+                | WindowChange::Floating
+                | WindowChange::Focus => {
+                    log::info!("received event {:?}", w.change);
+                    walk_tree(
+                        &Connection::new()?.get_tree()?,
+                        async move |node| -> anyhow::Result<()> {
+                            let conn = zbus::Connection::session().await?;
+                            let manager = ManagerProxy::new(&conn).await?;
+                            match node.node_type {
+                                NodeType::Con | NodeType::FloatingCon => {
+                                    let pid: u32 = node.pid.unwrap().try_into()?;
+                                    if let Ok(unit) = manager.get_unit_by_pid(pid).await {
+                                        let scope = ScopeProxy::builder(&conn)
+                                            .path(unit.clone())?
+                                            .build()
+                                            .await?;
+                                        let unit =
+                                            UnitProxy::builder(&conn).path(unit)?.build().await?;
+                                        let quota = if node.visible.unwrap() {
+                                            u64::MAX
+                                        } else {
+                                            1e5 as u64
+                                        };
+                                        let current_quota: u64 =
+                                            scope.cpu_quota_per_sec_u_sec().await?;
+                                        if current_quota != quota {
+                                            log::info!(
+                                                "setting CPUQuotaPerSecUSec of {} from {} to {}",
+                                                unit.id().await?,
+                                                current_quota,
+                                                quota
+                                            );
+                                            unit.set_properties(
+                                                true,
+                                                vec![(
+                                                    "CPUQuotaPerSecUSec".to_string(),
+                                                    zvariant::Value::U64(quota).into(),
+                                                )],
+                                            )
+                                            .await?;
+                                        }
                                     } else {
-                                        1e5 as u64
-                                    };
-                                    unit.set_properties(
-                                        true,
-                                        vec![(
-                                            "CPUQuotaPerSecUSec".to_string(),
-                                            zvariant::Value::U64(quota).into(),
-                                        )],
-                                    )
-                                    .await
-                                    .unwrap();
+                                        log::info!("no matching unit found for pid {}", pid);
+                                    }
+                                    Ok(())
+                                }
+                                _ => {
+                                    log::debug!(
+                                        "ignored node {:?}:{:?}",
+                                        node.node_type,
+                                        node.name
+                                    );
+                                    Ok(())
                                 }
                             }
-                            _ => (),
-                        }
-                    })
-                    .await;
+                        },
+                    )
+                    .await?;
                 }
-                _ => (),
+                _ => log::debug!("ignored event {:?}", w.change),
             },
             _ => unreachable!(),
         }
